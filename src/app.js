4dc3a1faed44c6ba34c26b751c72057eee74132b
Kaybel <kblmrtnz@gmail.com>
2018-06-20 09:54:01 -0400

    proyecto 2 susbtring y dropdown con nombres que se imprimen en html

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('button');
+const container = document.getElementById('root');
+const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+
+fetch(usersJSON)
+.then (response => response.json())
+.then(data => {
+  console.log(data);
+  renderUsers(data);
+})
+
+const renderUsers = data => {
+  btn.addEventListener('click', () => {
+    // forIn recorre objetos y accede a las propiedades
+    // for y forEach recorre arrays
+    const render = data.forEach(element => {
+      return container.innerHTML += `<p>${element.name}</p>`
+    })
+    return render;
+  })
+}
